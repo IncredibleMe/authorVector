f32c087741c39b07e8f332e4cf382319c2cb2de5
jack <alfa@aegean.gr>
2019-06-03 19:54:20 +0300

    fixed rate

--- mainGuardian.py
+++ mainGuardian.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import codecs
 from os import listdir
 from os.path import isfile, join, basename
@@ -7,11 +8,13 @@ import os
 import nltk.data
 import numpy
 import pathlib
+from nltk import ngrams
+from nltk.tokenize import word_tokenize
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 from sklearn import svm, metrics
 
 docLabels = []
-folder_name = "Guardian10/Politics"
+folder_name = "Guardian10/UK"
 authors = []
 
 doc_list = []
@@ -19,19 +22,25 @@ dir_list = []
 file_list = []
 i = 10
 
+
+def get_ngrams(text,n):
+    n_grams=ngrams(word_tokenize(text),n)
+    return [' '.join(grams) for grams in n_grams]
+
+
 #gia ka8e arxeio poy vrisketai ston fakelo C10train pare tin dieu8unsi tou arxeiou
 for dirpath, dirnames, filenames in os.walk(folder_name):
     #for subdirname in dirnames:
-        #authors.append(subdirname)
+    #authors.append(subdirname)
     for name in filenames:
         file_list.append(pathlib.PurePath(dirpath, name))
 
 #gia ka8e arxeio anoi3e to kai spase to arxeio se protaseis kai topo8etis
 for file in file_list:
-    f1 = codecs.open(file, "r", "utf-8", errors='ignore')
+    f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
     st = f1.read()
 
-    authors.append(basename(os.path.abspath(os.path.join(file, os.pardir))))
+    authors.append(basename(os.path.abspath(os.path.join(str(file), os.pardir))))
 
     #with codecs.open(file, "r",encoding='utf-8', errors='ignore') as st:
     #st = open(str(file),'r').read().decode('unicode_escape').encode('utf-8')
@@ -45,28 +54,30 @@ taggeddoc=[]
 
 
 for doc, index in zip(doc_list, docLabels):
-    td = TaggedDocument(words=nltk.word_tokenize(doc), tags=[str(index)])
+    # td = TaggedDocument(words=nltk.word_tokenize(doc), tags=[str(index)])
+    td = TaggedDocument(words=get_ngrams(doc,1), tags=[str(index)])
     taggeddoc.append(td)
 
-model = gensim.models.Doc2Vec(taggeddoc,  dm = 0, alpha=0.025, size= 10, min_alpha=0.025, min_count=0)
+model = gensim.models.Doc2Vec(taggeddoc,  dm = 0, alpha=0.025, vector_size= 10, min_alpha=0.025, min_count=0)
 
 
-# for epoch in range(10):
-#     if epoch % 2 == 0:
-#         print ('Now training epoch %s'%epoch)
-#     model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
-#     model.alpha -= 0.002  # decrease the learning rate
-#     model.min_alpha = model.alpha  # fix the learning rate, no decay
-#     model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
+for epoch in range(10):
+    if epoch % 2 == 0:
+        print ('Now training epoch %s'%epoch)
+    model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
+    model.alpha -= 0.002  # decrease the learning rate
+    model.min_alpha = model.alpha  # fix the learning rate, no decay
+    model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
 
 
-#model.save("doc2vec2.model")
+model.save("doc2vec2.model")
+
 model = Doc2Vec.load("doc2vec.model")
 
 all_vectors=[]
 
 for file in file_list:
-    f1 = codecs.open(file, "r", "utf-8", errors='ignore')
+    f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
     st = f1.read()
     #xorizoume to keimeno se protaseis
     sents = nltk.sent_tokenize(st)
@@ -89,12 +100,27 @@ for file in file_list:
 #     result.append(model.docvecs[string])
 
 
-svc = svm.SVC(kernel='linear', C=1)
+#
+kernels = ['linear', 'rbf', 'poly']
+cs = [0.1, 1, 10, 100, 1000]
+gammas = [0.1, 1, 10, 100]
+
+
+for kernel in kernels:
+    for c in cs:
+        for gam in gammas:
+            svc = svm.SVC(kernel=kernel, C=c, gamma=gam).fit(all_vectors,authors)
+            print(kernel+" |"+str(c)+" |"+str(gam))
+            print((svc.score(all_vectors,authors))*100)
+
+svc = svm.SVC(kernel='linear', C=1000,gamma=100)
 svc.fit(all_vectors,authors)
 
+print("----------------------------")
 
-folder_name = "Guardian10/World"
+folder_name = "Guardian10/Books"
 list_of_files =0
+
 #gia ka8e arxeio poy vrisketai ston fakelo C10train pare tin dieu8unsi tou arxeiou
 for dirpath, dirnames, filenames in os.walk(folder_name):
     for name in filenames:
@@ -105,7 +131,7 @@ for dirpath, dirnames, filenames in os.walk(folder_name):
 
 #gia ka8e arxeio anoi3e to kai spase to arxeio se protaseis kai topo8etis
 for file in file_list:
-    f1 = codecs.open(file, "r", "utf-8", errors='ignore')
+    f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
     st = f1.read()
     sent_text = nltk.sent_tokenize(st)
     for sentence in sent_text:
@@ -116,11 +142,10 @@ for file in file_list:
 all_vectors=[]
 
 for file in file_list:
-    f1 = codecs.open(file, "r", "utf-8", errors='ignore')
+    f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
     st = f1.read()
     #xorizoume to keimeno se protaseis
     sents = nltk.sent_tokenize(st)
-
 
 
     sentences = []
@@ -131,16 +156,20 @@ for file in file_list:
         new_vector = model.infer_vector(doc)
     all_vectors.append(new_vector)
 
+print (len(authors))
+
 predicted = svc.predict(all_vectors)
+print ("predicted" + str(len(predicted)))
 #print (predicted)
 
 list_common = []
 for a, b in zip(authors, predicted):
     if a == b:
         list_common.append(a)
+print (len(list_common))
 print (list_common)
-print ("Swsta vre8ikan %d apo ta ", len(list_common))
+print ("Swsta vre8ikan "+str(len(list_common))+" apo ta "+str(len(authors)))
 
-pososto = len(list_common)/list_of_files
+pososto = len(list_common)/len(authors)
 
-print ("%s epitixia", pososto)
+print ("epitixia : " , pososto)
