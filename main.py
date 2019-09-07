a1acabc1e280780d02ee2c0f429519d4766c1542
jack <alfa@aegean.gr>
2019-09-07 13:02:58 +0300

    working perfect

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from os import listdir
 from os.path import isfile, join
 import os
@@ -6,9 +7,13 @@ import os
 import nltk.data
 import numpy
 import pathlib
+from nltk import ngrams
+from nltk.tokenize import word_tokenize
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 from sklearn import svm, metrics
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+nltk.download('punkt')
 docLabels = []
 folder_name = "C10/C10train"
 authors = []
@@ -17,6 +22,12 @@ doc_list = []
 dir_list = []
 file_list = []
 i = 10
+
+
+def get_ngrams(text,n):
+    n_grams=ngrams(word_tokenize(text),n)
+    return [' '.join(grams) for grams in n_grams]
+
 #gia ka8e arxeio poy vrisketai ston fakelo C10train pare tin dieu8unsi tou arxeiou
 for dirpath, dirnames, filenames in os.walk(folder_name):
     for subdirname in dirnames:
@@ -38,25 +49,26 @@ taggeddoc=[]
 
 
 
-
-
 for doc, index in zip(doc_list, docLabels):
-    td = TaggedDocument(words=nltk.word_tokenize(doc), tags=[str(index)])
+    # td = TaggedDocument(words=nltk.word_tokenize(doc), tags=[str(index)])
+    td = TaggedDocument(words=get_ngrams(doc,4), tags=[str(index)])
     taggeddoc.append(td)
 
 model = gensim.models.Doc2Vec(taggeddoc,  dm = 0, alpha=0.025, size= 20, min_alpha=0.025, min_count=0)
 
 
-# for epoch in range(10):
-#     if epoch % 2 == 0:
-#         print ('Now training epoch %s'%epoch)
-#     model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
-#     model.alpha -= 0.002  # decrease the learning rate
-#     model.min_alpha = model.alpha  # fix the learning rate, no decay
-#     model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
-#
-#
-# model.save("doc2vec.model")
+for epoch in range(10):
+    if epoch % 2 == 0:
+        print ('Now training epoch %s'%epoch)
+    model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
+    model.alpha -= 0.002  # decrease the learning rate
+    model.min_alpha = model.alpha  # fix the learning rate, no decay
+    model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
+
+
+
+model.save("doc2vec.model")
+
 model = Doc2Vec.load("doc2vec.model")
 
 all_vectors=[]
@@ -75,17 +87,40 @@ for file in file_list:
     all_vectors.append(new_vector)
 
 
-
+# kernels = ['linear', 'rbf', 'poly']
+# cs = [0.1, 1, 10, 100, 1000]
+# gammas = [0.1, 1, 10, 100]
 # #SVM classification
 # result = []
 # for i in range(1,10684):
 #     string = str(i)
 #     result.append(model.docvecs[string])
 
+# X_train, X_test, y_train, y_test = train_test_split(
+#     all_vectors, authors, test_size=0.2, random_state=42
+# )
 
-svc = svm.SVC(kernel='linear', C=1)
-svc.fit(all_vectors,authors)
+# for kernel in kernels:
+#     for c in cs:
+#         for gam in gammas:
+#             svc = svm.SVC(kernel=kernel, C=c, gamma=gam).fit(X_train, y_train)
+#             print(kernel+" |"+str(c)+" |"+str(gam))
+#             print((svc.score(X_test, y_test))*100)
 
+
+svc = svm.SVC(kernel='rbf', C=1000, gamma=10).fit(all_vectors, authors) #97,6%
+
+# svc = svm.SVC(kernel='linear', C=1000, gamma=10).fit(X_train, y_train)   /// 73,8% akrivia
+# svc.fit(all_vectors,authors)
+# print((svc.score(X_test, y_test))*100)
+# svc.fit(X_train, y_train)
+
+# pred = svc.predict(X_test)
+# print((svc.score(X_test, y_test))*100)
+# print(confusion_matrix(pred, y_test))
+
+
+print("-------------------------------------------------")
 
 folder_name = "C10/C10test"
 
@@ -118,7 +153,7 @@ for file in file_list:
         new_vector = model.infer_vector(doc)
     all_vectors.append(new_vector)
 
-predicted = svc.predict(all_vectors)
+#predicted = svc.predict(2)
 #print (predicted)
 
 list_common = []
@@ -126,8 +161,8 @@ for a, b in zip(authors, predicted):
     if a == b:
         list_common.append(a)
 print (list_common)
-print ("Swsta vre8ikan %d apo ta 500", len(list_common))
+print ("Swsta vre8ikan "+str(len(list_common))+" apo ta 500" )
 
 pososto = len(list_common)/500
 
-print ("%s epitixia", pososto)
+print str(pososto) + " epitixia"
--- mainGuardian.py
+++ mainGuardian.py
@@ -12,9 +12,12 @@ from nltk import ngrams
 from nltk.tokenize import word_tokenize
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 from sklearn import svm, metrics
+import multiprocessing
+cores = multiprocessing.cpu_count()
 
 docLabels = []
-folder_name = "Guardian10/UK"
+folder_name = "Guardian10/World"
+test_folder = "Guardian10/Books"
 authors = []
 
 doc_list = []
@@ -25,25 +28,24 @@ i = 10
 
 def get_ngrams(text,n):
     n_grams=ngrams(word_tokenize(text),n)
+    print ([' '.join(grams) for grams in n_grams])
     return [' '.join(grams) for grams in n_grams]
 
 
-#gia ka8e arxeio poy vrisketai ston fakelo C10train pare tin dieu8unsi tou arxeiou
+# gia ka8e arxeio poy vrisketai ston fakelo C10train
+# pare tin dieu8unsi tou arxeiou
 for dirpath, dirnames, filenames in os.walk(folder_name):
-    #for subdirname in dirnames:
-    #authors.append(subdirname)
     for name in filenames:
         file_list.append(pathlib.PurePath(dirpath, name))
 
-#gia ka8e arxeio anoi3e to kai spase to arxeio se protaseis kai topo8etis
+# gia ka8e arxeio apo tin parapanw lista
+# anoi3e to kai spase to arxeio se protaseis kai topo8etisetes se lista
 for file in file_list:
     f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
     st = f1.read()
 
     authors.append(basename(os.path.abspath(os.path.join(str(file), os.pardir))))
 
-    #with codecs.open(file, "r",encoding='utf-8', errors='ignore') as st:
-    #st = open(str(file),'r').read().decode('unicode_escape').encode('utf-8')
     sent_text = nltk.sent_tokenize(st)
     for sentence in sent_text:
         doc_list.append(sentence)
@@ -54,11 +56,13 @@ taggeddoc=[]
 
 
 for doc, index in zip(doc_list, docLabels):
-    # td = TaggedDocument(words=nltk.word_tokenize(doc), tags=[str(index)])
-    td = TaggedDocument(words=get_ngrams(doc,1), tags=[str(index)])
+    #print(nltk.word_tokenize(doc))
+    td = TaggedDocument(words=nltk.word_tokenize(doc), tags=[str(index)])
+    #td = TaggedDocument(words=get_ngrams(doc,4), tags=[str(index)])
     taggeddoc.append(td)
 
-model = gensim.models.Doc2Vec(taggeddoc,  dm = 0, alpha=0.025, vector_size= 10, min_alpha=0.025, min_count=0)
+#dimiourgia montelou apo to sunolo protasewn twn keimenwn
+model = gensim.models.Doc2Vec(taggeddoc,  dm = 0,window=2, alpha=0.025, vector_size= 300, min_alpha=0.050, min_count=2, workers =cores)
 
 
 for epoch in range(10):
@@ -70,7 +74,7 @@ for epoch in range(10):
     model.train(taggeddoc, total_examples=model.corpus_count, epochs=model.iter)
 
 
-model.save("doc2vec2.model")
+model.save("doc2vec.model")
 
 model = Doc2Vec.load("doc2vec.model")
 
@@ -86,12 +90,11 @@ for file in file_list:
 
     for sen in sents:
         doc = nltk.word_tokenize(sen)
-        sentences.append(doc)
+        #sentences.append(doc)
         #print sentences
-        new_vector = model.infer_vector(doc)
+        sentences.extend(doc)
+        new_vector = model.infer_vector(sentences)
     all_vectors.append(new_vector)
-
-
 
 # #SVM classification
 # result = []
@@ -100,11 +103,59 @@ for file in file_list:
 #     result.append(model.docvecs[string])
 
 
-#
 kernels = ['linear', 'rbf', 'poly']
 cs = [0.1, 1, 10, 100, 1000]
 gammas = [0.1, 1, 10, 100]
 
+
+
+
+
+svc = svm.SVC(kernel='linear', C=1000, gamma=10)
+svc.fit(all_vectors,authors)
+
+print("----------------------------")
+
+
+list_of_files =0
+file_list = []
+#gia ka8e arxeio poy vrisketai ston fakelo C10train pare tin dieu8unsi tou arxeiou
+for dirpath, dirnames, filenames in os.walk(test_folder):
+    for name in filenames:
+        #metraei to plithos twn arxeiwn sto test
+        list_of_files += 1
+        file_list.append(pathlib.PurePath(dirpath, name))
+
+
+# #gia ka8e arxeio anoi3e to kai spase to arxeio se protaseis kai topo8etis
+# for file in file_list:
+#     f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
+#     st = f1.read()
+#     sent_text = nltk.sent_tokenize(st)
+#     for sentence in sent_text:
+#         doc_list.append(sentence)
+#         docLabels.append(i)
+#         i=i+1
+
+all_vectors=[]
+authors = []
+for file in file_list:
+    f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
+    st = f1.read()
+    authors.append(basename(os.path.abspath(os.path.join(str(file), os.pardir))))
+    #xorizoume to keimeno se protaseis
+    sents = nltk.sent_tokenize(st)
+
+
+    sentences = []
+    for sen in sents:
+        #xwrizoume to keimeno se lexeis
+        doc = nltk.word_tokenize(sen)
+        #sentences.append(doc)
+        sentences.extend(doc)
+    new_vector = model.infer_vector(sentences)
+    #print (sentences)
+    all_vectors.append(new_vector)
 
 for kernel in kernels:
     for c in cs:
@@ -113,59 +164,17 @@ for kernel in kernels:
             print(kernel+" |"+str(c)+" |"+str(gam))
             print((svc.score(all_vectors,authors))*100)
 
-svc = svm.SVC(kernel='linear', C=1000,gamma=100)
-svc.fit(all_vectors,authors)
-
-print("----------------------------")
-
-folder_name = "Guardian10/Books"
-list_of_files =0
-
-#gia ka8e arxeio poy vrisketai ston fakelo C10train pare tin dieu8unsi tou arxeiou
-for dirpath, dirnames, filenames in os.walk(folder_name):
-    for name in filenames:
-        #metraei to plithos twn arxeiwn sto test
-        list_of_files += 1
-        file_list.append(pathlib.PurePath(dirpath, name))
-
-
-#gia ka8e arxeio anoi3e to kai spase to arxeio se protaseis kai topo8etis
-for file in file_list:
-    f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
-    st = f1.read()
-    sent_text = nltk.sent_tokenize(st)
-    for sentence in sent_text:
-        doc_list.append(sentence)
-        docLabels.append(i)
-        i=i+1
-
-all_vectors=[]
-
-for file in file_list:
-    f1 = codecs.open(str(file), "r", "utf-8", errors='ignore')
-    st = f1.read()
-    #xorizoume to keimeno se protaseis
-    sents = nltk.sent_tokenize(st)
-
-
-    sentences = []
-    for sen in sents:
-        doc = nltk.word_tokenize(sen)
-        sentences.append(doc)
-        #print sentences
-        new_vector = model.infer_vector(doc)
-    all_vectors.append(new_vector)
-
-print (len(authors))
-
+print ("authors" + str(len(authors)))
+#print ("All " + str(len(all_vectors)))
 predicted = svc.predict(all_vectors)
 print ("predicted" + str(len(predicted)))
-#print (predicted)
+
 
 list_common = []
 for a, b in zip(authors, predicted):
     if a == b:
         list_common.append(a)
+
 print (len(list_common))
 print (list_common)
 print ("Swsta vre8ikan "+str(len(list_common))+" apo ta "+str(len(authors)))
